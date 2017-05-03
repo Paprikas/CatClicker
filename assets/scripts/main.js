3597610c4dafac664347e4b8b0ea19bef9045aa5
Maxim Zelenkin <nudepatch@gmail.com>
2017-05-03 17:02:48 +0300

    Split View on 2 views
    + Admin view

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,35 +44,59 @@ var model = {
 var controller = {
   init: function () {
     model.init()
-    view.init()
+    adminView.init()
+    catView.init()
+    catListView.render()
   },
   getCats: function () {
     return model.cats
   },
   incrementClicks: function () {
     model.currentCat.clicks++
-    view.renderToMain(model.currentCat)
+    catView.render(model.currentCat)
   },
   setCurrentCat: function (cat) {
     model.currentCat = cat
   },
   getCurrentCat: function () {
     return model.currentCat
+  },
+  updateCurrentCat: function () {
+    model.currentCat.name = adminView.name.value
+    model.currentCat.image = adminView.image.value
+    model.currentCat.clicks = adminView.clicks.value
+    catView.render(model.currentCat)
+    adminView.hide()
   }
 }
 
-var view = {
+var catView = {
   init: function () {
-    this.catClicks = document.getElementById('main-cat-clicks')
-    this.catName = document.getElementById('main-cat-name')
-    this.catImage = document.getElementById('main-cat-image')
+    this.catClicks = document.getElementById('cat-clicks')
+    this.catName = document.getElementById('cat-name')
+    this.catImage = document.getElementById('cat-image')
+    this.adminButton = document.getElementById('show-admin')
     this.catImage.addEventListener('click', function () {
       controller.incrementClicks()
     })
-
-    this.renderList()
+    this.adminButton.addEventListener('click', function () {
+      adminView.render()
+    })
   },
-  renderList: function () {
+  render: function () {
+    var cat = controller.getCurrentCat()
+    var catContainer = document.getElementById('cat')
+    catContainer.removeAttribute('class')
+    this.catClicks.innerText = cat.clicks
+    this.catName.innerText = cat.name
+    this.catImage.setAttribute('src', './assets/images/' + cat.image)
+  }
+}
+
+var catListView = {
+  render: function () {
+    var catsContainer = document.getElementById('cats-list')
+
     for (var i = 0; i < controller.getCats().length; i++) {
       var cat = controller.getCats()[i]
 
@@ -83,19 +107,45 @@ var view = {
         function (cat) {
           return function () {
             controller.setCurrentCat(cat)
-            view.renderToMain()
+            catView.render()
+            adminView.hide()
           }
         }
       )(cat))
 
-      document.body.appendChild(paragraph)
+      catsContainer.appendChild(paragraph)
     }
+  }
+}
+
+var adminView = {
+  init: function () {
+    this.adminContainer = document.getElementById('admin-container')
+    this.name = document.getElementById('a-name')
+    this.image = document.getElementById('a-image')
+    this.clicks = document.getElementById('a-clicks')
+    this.cancelButton = document.getElementById('cancel-button')
+    this.updateButton = document.getElementById('update-button')
+
+    this.cancelButton.addEventListener('click', function () {
+      adminView.hide()
+    })
+    this.updateButton.addEventListener('click', function () {
+      controller.updateCurrentCat()
+    })
   },
-  renderToMain: function () {
+  render: function () {
     var cat = controller.getCurrentCat()
-    this.catClicks.innerText = cat.clicks
-    this.catName.innerText = cat.name
-    this.catImage.setAttribute('src', './assets/images/' + cat.image)
+    this.show()
+    this.name.value = cat.name
+    this.image.value = cat.image
+    this.clicks.value = cat.clicks
+  },
+  hide: function () {
+    this.adminContainer.setAttribute('class', 'hidden')
+  },
+  show: function () {
+    this.adminContainer.removeAttribute('class')
   }
 }
 
